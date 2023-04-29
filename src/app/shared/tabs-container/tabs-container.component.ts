54a3375e37523d9eb4be1c1d0b81f0398fbdbe35
jaymeeu <rasaqadewuyi@gmail.com>
2023-04-29 18:23:15 +0100

    tab component

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,11 @@
+import { Component, ContentChildren } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent  {
+  @ContentChildren(TabComponent) tabs = {}
+}
