1fc88c4ea546f62470833e761a6deb8855e7eaaf
jaymeeu <rasaqadewuyi@gmail.com>
2023-05-01 21:47:49 +0100

    adding firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : true,
+    firebase : {
+        apiKey: "AIzaSyC2KFczCXKu2UMRUcVzuNJgyk_NECaJwmM",
+        authDomain: "clips-939c5.firebaseapp.com",
+        projectId: "clips-939c5",
+        storageBucket: "clips-939c5.appspot.com",
+        messagingSenderId: "736752309148",
+        appId: "1:736752309148:web:6899afbaa24a1ec4342e59"
+      }
+
+}
